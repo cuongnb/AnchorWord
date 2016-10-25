75b30f9bc406d3881d34a6af8b3a9fc23ac5a6c4
cuongtt <cuongtrantien@admicro.vn>
2016-10-25 21:29:42 +0700

    Updating

--- AnchorWord/AnchorWord.py
+++ AnchorWord/AnchorWord.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from pyhull import convex_hull
+
+from sklearn import decomposition
+from sklearn.manifold import TSNE
+
+
 def constructQMatrix(data_path):
     # Read data
     data = []
@@ -55,14 +64,78 @@ def constructQMatrix(data_path):
     # print "Q matrix: "
     # for item in Q.tolist():
     #     print item
-    return Q.tolist()
+    return Q.tolist(), rwordID
+
+
+def tnse(data, n_com):
+    tnse = TSNE(n_components=n_com)
+    shrink_data = tnse.fit_transform(data)
+    return shrink_data
+
+
+def pca(data, n_com):
+    pca = decomposition.PCA(n_components=n_com)
+    shrink_data = pca.fit_transform(data)
+    return shrink_data
+
+
+def quickhull(data, n_com):
+    convexhull = convex_hull.ConvexHull(data)
+    res = []
+    anchor = []
+    for item in convexhull.simplices:
+        res.append(item.coords)
+        for i in range(n_com):
+            p = []
+            for j in range(n_com):
+                p.append(item.coords[i][j])
+            anchor.append(p)
+
+    return res, anchor
 
 
 def main():
-    data_path = '/home/cuongtt/PycharmProjects/AnchorWord/Data/tiny.anchor'
-    Q = constructQMatrix(data_path)
-    for item in Q:
-        print item
+    # Build word-occurrence matrix
+    data_path = '/home/cuong/PycharmProjects/AnchorWord/Data/tiny.anchor'
+    Q, idword = constructQMatrix(data_path)
+    print 'Number of word: ' + str(len(idword))
+    # for item in Q:
+    #     print item
+    # for item in idword:
+    #     print str(item) + ': ' + str(idword[item])
+
+    # Project data into low-dimension space
+    n_components = 3
+    new_data = tnse(Q, n_components)
+    # print new_data
+
+    # Find convex hull on low-dimension data
+    hull, anchor = quickhull(new_data, n_components)
+
+    # for item in anchor:
+    #     print item
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for i in range(len(new_data)):
+        item = new_data[i]
+        ax.scatter(item[0], item[1], item[2], color='r', marker='o', label=i)
+        if list(item) in anchor:
+            ax.text(item[0], item[1], item[2],  '%s' % (idword[i]), size=20, zorder=1, color='k')
+
+    for item in hull:
+        if [item[0][0], item[0][1], item[0][2]] not in anchor:
+            anchor.append([item[0][0], item[0][1], item[0][2]])
+        if [item[1][0], item[1][1], item[1][2]] not in anchor:
+            anchor.append([item[1][0], item[1][1], item[1][2]])
+        if [item[2][0], item[2][1], item[2][2]] not in anchor:
+            anchor.append([item[2][0], item[2][1], item[2][2]])
+        ax.plot([item[0][0], item[1][0]], [item[0][1], item[1][1]], [item[0][2], item[1][2]], color='b')
+        ax.plot([item[1][0], item[2][0]], [item[1][1], item[2][1]], [item[1][2], item[2][2]], color='b')
+        ax.plot([item[2][0], item[0][0]], [item[2][1], item[0][1]], [item[2][2], item[0][2]], color='b')
+    # plt.legend()
+    plt.show()
+    print 'Number of anchor word: ' + str(len(anchor))
 
 if __name__ == '__main__':
     main()
